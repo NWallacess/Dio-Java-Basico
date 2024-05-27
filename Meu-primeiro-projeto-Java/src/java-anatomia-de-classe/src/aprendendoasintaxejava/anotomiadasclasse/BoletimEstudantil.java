526801049ff6ba468f84ecc7fad9753a2fe7da6d
NWallacess <wallacenicholas84@gmail.com>
2024-05-27 20:41:42 -0300

    commint natomia das classe

---
+++ Meu-primeiro-projeto-Java/src/java-anatomia-de-classe/src/aprendendoasintaxejava/anotomiadasclasse/BoletimEstudantil.java
@@ -0,0 +1,12 @@
+package aprendendoasintaxejava.anotomiadasclasse;
+// arquivo BoletimEstudantil.java
+public class BoletimEstudantil {
+    public static void main(String[] args) {int mediaFinal = 6;
+		if (mediaFinal < 7)
+			System.out.println("REPROVADO");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");	
+    }
+}
